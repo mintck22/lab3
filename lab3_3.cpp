efa47448145c26c2e1d98844891e01f908830f54
Mint <chawanluck_k@hotmail.com>
2019-12-27 16:59:50 +0700

    second

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -4,15 +4,14 @@ using namespace std;
 
 int main()
 {
-	float x = 2;
+	float x = 6;
 	float y , sum = 0;
 	
 	
-	while(x<=69){
+	while(x <= 19){
 		y=1/x;
 		sum = sum+y;
 		x=x+1;
-		
 	}
 	
 	cout << "sum = " << sum;
